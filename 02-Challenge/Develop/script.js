0d860d8e1f4a77b045e012d1def6705ed5c83a31
Kevin Quinlan <quinlan72@aol.com>
2022-08-02 15:17:57 -0500

    fixed for loop and variables

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,13 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
-var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var specialCharacters = "~`!@#$%^&*()-_=+"
-
-var allOptions = [lowercaseAlphabet, uppercaseAlphabet, numbers, specialCharacters]
-
-
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "~`!@#$%^&*()-_=+";
+var passwordString = "";
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
@@ -16,23 +14,41 @@ function writePassword() {
   var uppercaseAlpha = confirm("Would you like to include uppercase letters?");
   var numb = confirm("Would you like to include any numbers?")
   var specialChara = confirm("Would you like to include any special characters?");
-  var passwordLen = confirm("Please choose between 8 and 128 characters.");
-  var passwordLength = 129; 
+  var passwordLength = (prompt("Please choose between 8 and 128 characters.")); 
 
-
-
-
-  for (var i = 9; i <= passwordLength; i++) {
-    var randomString = Math.floor(Math.random() * )
+  while(passwordLength <= 9 || passwordLength >= 129) {
+    alert("Password length must be between 8 and 128 characters. Please try again")
+    var passwordLength = (prompt("Please choose between 8 and 128 characters.")); 
   }
 
-  var password = "";
-  var passwordText = document.querySelector("#password");
+  var passwordString = "";
 
+  if (lowercaseAlpha) {
+    passwordString += lowercaseAlphabet;
+  }
 
-  passwordText.value = password;
+  if (uppercaseAlpha) {
+    passwordString += uppercaseAlphabet;
+  }
+
+  if (numb) {
+    passwordString += numbers;
+  }
+
+  if (specialChara) {
+    passwordString += specialCharacters;
+  }
+
+  var newPassword = ""
+
+  for (var i = 1; i <= passwordLength; i++) {
+    newPassword += passwordString.charAt(Math.floor(Math.random() * passwordString.length));
+  } 
+
+  passwordText.value = newPassword; 
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
